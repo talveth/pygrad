fixed .sum() to accept any axes, fixed .std() outputting nan gradients for length 1 axes

autograd/cpu/losses.py
@@ -21,7 +21,7 @@ class BCELoss:
         assert np.max(pred.value)   <= 1 and np.min(pred.value) >= 0, "Ensure predictions have value in [0,1]"
         assert np.max(target.value) <= 1 and np.min(target.value) >= 0, "Ensure targets have values in {0,1}"
         outp          = target * pred.log() + (1-target) * ((1-pred).log())       # is batched
-        loss          = (-1/pred.shape[0]) * outp.sum(over_batch=True)            # sums in batch dimension also
+        loss          = (-1/pred.shape[0]) * outp.sum(axis=(0,1,2))            # sums in batch dimension also
         return loss
 
 
@@ -44,7 +44,7 @@ class CCELoss:
             target = target.reshape((-1,1,target.shape[-1]))
         
         if not mask:
-            loss             = (target * (pred.softmax().log())).sum(over_batch=True)
+            loss             = (target * (pred.softmax().log())).sum(axis=(0,1,2))
             loss_multiplier  = Tensor(value=np.ones_like(loss.value)*(-1/pred.shape[0]))
             loss             = loss * loss_multiplier
         else:
@@ -52,7 +52,7 @@ class CCELoss:
             mask_np          = (target[:,:,0] != 1)[...,None]
             eos_no_mask      = ((target[:,:,0] == 1).cumsum(axis=1) == 1)[...,None]
             mask             = Tensor(mask_np | eos_no_mask, learnable=False, leaf=True)
-            loss             = ((target*mask)*(pred.softmax().log())).sum(over_batch=True)
+            loss             = ((target*mask)*(pred.softmax().log())).sum(axis=(0,1,2))
             loss_multiplier  = Tensor(value=np.ones_like(loss.value)*(-1/np.sum(mask[:])))
             loss             = loss * loss_multiplier
         return loss

autograd/cpu/tensor.py
@@ -153,7 +153,7 @@ class Tensor:
     def __rsub__(self, other):
         return (-self) + other
 
-    def sum(self, over_batch:bool=True):
+    def sum(self, axis:Union[None,int,tuple]=None, keepdims:bool=False):
         """
         Returns a Tensor with value equal to the sum of all the Tensor values.
 
@@ -161,22 +161,27 @@ class Tensor:
         dimension, and instead the sum is taken over the other dimensions.
         
         over_batch: boolean
+        axis: None, int, or tuple of ints, suitable for self.shape. 
+                    If None, sum is performed over all axes.
+        keepdims:bool, False
         """
-        assert isinstance(over_batch, bool), "over_batch must be a boolean."
-        if over_batch:
-            new_val         = np.einsum('...->', self.value, optimize='optimal')
-        else:
-            new_val         = np.einsum('i...->i', self.value, optimize='optimal')
-        
-        # if over_batch:
-            # new_val = new_val.reshape(1, 1, -1)
-        # else:
-            # reshape_dims = (self.shape[0], 1, -1) if len(self.shape) <= 3 else (self.shape[0], 1, 1, -1)
-            # new_val = new_val.reshape(reshape_dims)
+        assert isinstance(keepdims, bool), "keepdims must be a boolean."
+        assert isinstance(axis, (int, tuple, type(None))), "axis must be None, an int, or a tuple."
 
-        new             = Tensor(value=new_val, _prev=(self,), learnable=True, leaf=True)
+        if axis is None:
+            axis = tuple([i for i in range(len(self.shape))])
+        new_val             = np.sum(self.value, axis=axis, keepdims=keepdims)
+        new                 = Tensor(value=new_val, _prev=(self,), learnable=True, leaf=True)
         def bpass():
-            self.grad  += np.einsum('...,...->...', np.ones(self.shape), new.grad, optimize='optimal')
+            if keepdims:
+                self.grad  += np.einsum('...,...->...', np.ones(self.shape), new.grad, optimize='optimal')
+            else:
+                new_grad    = np.expand_dims(new.grad, axis=axis)
+                self.grad  += np.einsum('...,...->...', np.ones(self.shape), new_grad, optimize='optimal')
+            # print(new.grad.shape, self.shape, self.grad.shape)
+            # self_grad   = 
+            # self.grad  += 
+            # self.grad  += np.einsum('...,...->...', np.ones(self.shape), new.grad, optimize='optimal')
         new.bpass       = bpass
         return new
 
@@ -254,17 +259,22 @@ class Tensor:
         new.bpass = bpass
         return new
 
-    def std(self, axis:int):
+    def std(self, axis:int, keepdim=True):
         """
         Returns the standard deviation of the Tensor along the specified axis.
         Bias correction is not performed here.
 
         axis: int
+        keepdim: bool
         """
-        assert isinstance(axis, int), "The specified axis must be an integer."
+        assert isinstance(axis, int),       "The specified axis must be an integer."
+        assert isinstance(keepdim, bool),   ""
         N   = self.value.shape[axis]
+        if N == 1: return 0 * self
         d2  = (self - self.mean(axis=axis,keepdims=True))**2  # abs is for complex `a`
-        var = d2.mean(axis=axis, keepdims=True)               # no bias correction done here
+        print(d2.value)
+        var = d2.mean(axis=axis, keepdims=keepdim)            # no bias correction done here
+        print(var.value)
         new = var**0.5
         return new
 

tests/test_tensor.py
@@ -33,7 +33,7 @@ class TestTensor(unittest.TestCase):
             # custom
             a                   = Tensor(a_val)
             b                   = Tensor(b_val)
-            tensor_loss         = (a@b).sum(over_batch=True)
+            tensor_loss         = (a@b).sum()
             tensor_loss.backward()
 
             # pytorch
@@ -88,13 +88,13 @@ class TestTensor(unittest.TestCase):
                     bt = torch.tensor(b_val, requires_grad=True)
                     
                     if k == 0:
-                        tensor_loss = (a+b).sum(over_batch=True)
+                        tensor_loss = (a+b).sum()
                         print((a+b).shape, shapes_1[i], shapes_2[i])
                         tensor_loss.backward()
                         torch_loss = (at+bt).sum()
                         torch_loss.backward()
                     elif k == 1:
-                        tensor_loss = (a-b).sum(over_batch=True)
+                        tensor_loss = (a-b).sum()
                         tensor_loss.backward()
                         torch_loss = (at-bt).sum()
                         torch_loss.backward()
@@ -186,13 +186,13 @@ class TestTensor(unittest.TestCase):
                     bt = torch.tensor(b_val, requires_grad=True)
                     
                     if k == 0:
-                        tensor_loss = (a*b).sum(over_batch=True)
+                        tensor_loss = (a*b).sum()
                         print((a+b).shape, shapes_1[i], shapes_2[i])
                         tensor_loss.backward()
                         torch_loss = (at*bt).sum()
                         torch_loss.backward()
                     elif k == 1:
-                        tensor_loss = (a/b).sum(over_batch=True)
+                        tensor_loss = (a/b).sum()
                         tensor_loss.backward()
                         torch_loss = (at/bt).sum()
                         torch_loss.backward()

tests/test_tensor_extras.py
@@ -13,10 +13,86 @@ import numba as nb
 
 
 class TestTensorExtras(unittest.TestCase):
+
+    def test_std(self):
+        shapes = [(10,20,20), (10,20), (1,1,1), (1,10,1), (1,1,1,1)]
+        for shape in shapes:
+            for axis in range(len(shape)):
+                for keepdims in [True, False]:
+
+                    a_val               = np.random.uniform(0, 1, shape)
+
+                    # custom
+                    a                   = Tensor(a_val)
+                    a_fnc               = a.std(axis=axis, keepdim=keepdims)
+                    tensor_loss         = (a_fnc).sum()
+                    tensor_loss.backward()
+
+                    # pytorch
+                    at                  = torch.tensor(a_val, requires_grad=True)
+                    at_fnc              = torch.std(at, dim=axis, keepdim=keepdims, correction=0)
+                    at_fnc.retain_grad()
+                    torch_loss          = (at_fnc).sum()
+                    torch_loss.backward()
+
+                    self.assertTrue(np.all(np.isclose(tensor_loss.value.item(), torch_loss.detach().numpy().item())))
+                    self.assertTrue(a.grad.shape == tuple(at.grad.shape))
+                    self.assertTrue(np.all(np.isclose(a.grad, at.grad, rtol=1e-5)))
+                    self.assertTrue(np.all(np.isclose(a_fnc.grad, at_fnc.grad, rtol=1e-5)))
+
+    def test_mean(self):
+        shapes = [(10,20,20), (10,20), (1,1,1), (1,10,1), (1,1,1,1)]
+        for shape in shapes:
+            for axis in range(len(shape)):
+                for keepdims in [True, False]:
+                    a_val               = np.random.uniform(0, 1, shape)
+
+                    # custom
+                    a                   = Tensor(a_val)
+                    a_fnc               = a.mean(axis=axis, keepdims=keepdims)
+                    tensor_loss         = (a_fnc).sum()
+                    tensor_loss.backward()
+
+                    # pytorch
+                    at                  = torch.tensor(a_val, requires_grad=True)
+                    at_fnc              = at.mean(axis=axis, keepdims=keepdims)
+                    at_fnc.retain_grad()
+                    torch_loss          = (at_fnc).sum()
+                    torch_loss.backward()
+
+                    self.assertTrue(tensor_loss.value, torch_loss.detach().numpy())
+                    self.assertTrue(a.grad.shape == tuple(at.grad.shape))
+                    self.assertTrue(np.all(np.isclose(a.grad, at.grad)))
+                    self.assertTrue(np.all(np.isclose(a_fnc.grad, at_fnc.grad)))
+
+    def test_sum(self):
+        shapes = [(10,20,20), (10,20), (1,1,1), (1,10,1), (1,1,1,1)]
+        for shape in shapes:
+            for axis in range(len(shape)):
+                for keepdims in [True, False]:
+                    a_val               = np.random.uniform(0, 1, shape)
+
+                    # custom
+                    a                   = Tensor(a_val)
+                    a_fnc               = a.sum(axis=axis, keepdims=keepdims)
+                    tensor_loss         = (a_fnc).sum()
+                    tensor_loss.backward()
+
+                    # pytorch
+                    at                  = torch.tensor(a_val, requires_grad=True)
+                    at_fnc              = at.sum(axis=axis, keepdims=keepdims)
+                    at_fnc.retain_grad()
+                    torch_loss          = (at_fnc).sum()
+                    torch_loss.backward()
+
+                    self.assertTrue(tensor_loss.value, torch_loss.detach().numpy())
+                    self.assertTrue(a.grad.shape == tuple(at.grad.shape))
+                    self.assertTrue(np.all(np.isclose(a.grad, at.grad)))
+                    self.assertTrue(np.all(np.isclose(a_fnc.grad, at_fnc.grad)))
+
     def test_relu(self):
         shapes = [(10,20,20), (10,20), (1,1,1), (), (1,10,1)]
         for shape in shapes:
-            # checks for broken broadcasting when doing mul ops
             a_val               = np.random.uniform(0, 1, shape)
 
             # custom

tests/test_transformer.py
@@ -396,14 +396,13 @@ class CCELoss:
             target = target.reshape((-1,1,target.shape[-1]))
         
         if not mask:
-            loss             = (target * (pred.softmax().log())).sum(over_batch=True)
+            loss             = (target * (pred.softmax().log())).sum()
             loss_multiplier  = Tensor(value=np.ones_like(loss.value)*(-1/pred.shape[0]))
             loss             = loss * loss_multiplier
         else:
-            # fix scalar issue
             mask_np          = (target[:,:,0] != 1)[...,None]
             mask             = Tensor(mask_np, learnable=False, leaf=True)
-            loss             = ((target*mask)*(pred.softmax().log())).sum(over_batch=True)
+            loss             = ((target*mask)*(pred.softmax().log())).sum()
             loss_multiplier  = Tensor(value=np.ones_like(loss.value)*(-1/np.sum(mask[:])))
             loss             = loss * loss_multiplier
         return loss