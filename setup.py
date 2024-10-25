c58f331a6fc16d6a33fb2e1694024ee794d4c150
Danila Kurganov <dan.kurg@gmail.com>
2024-10-25 18:02:50 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -4,22 +4,20 @@ from setuptools import setup, find_packages
 setup(
     name="danila_grad",
     version="0.0.1",
-    packages=find_packages(exclude=["_correctness*",
-                                    "_extras*",
-                                    "_speed*",
-                                    ".direnv",
+    packages=find_packages(exclude=[
+                                    # ".direnv",
                                     "data*",
                                     "examples",
                                     "tests",
-                                    ".envrc",
+                                    # ".envrc",
                                     ]),
     install_requires=[
-        "numpy",
-        "numba",
+        "numpy>=0.60.0",
+        "numba>=2.0.2",
     ],
     author="Danila Kurganov",             # Your name
     author_email="dan.kurg@gmail.com",  # Your email
-    description="Autograd in NumPy",
+    description="Automatic Differentiation in Python using Numpy/Numba",
     long_description=open("README.md").read(),
     url="https://github.com/baubels/danila-grad",
     classifiers=[
