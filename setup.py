35e92c501296b35ea51312e4473e07d22b7e4cee
Danila Kurganov <dan.kurg@gmail.com>
2024-11-28 23:39:11 +0100

    modified readme and updated setup.py

--- setup.py
+++ setup.py
@@ -5,26 +5,33 @@ setup(
     name="danila_grad",
     version="0.0.1",
     packages=find_packages(exclude=[
-                                    # ".direnv",
-                                    "data*",
                                     "examples",
                                     "tests",
-                                    # ".envrc",
+                                    "docs",
                                     ]),
     install_requires=[
         "numpy>=0.60.0",
         "numba>=2.0.2",
+        "opt_einsum>=3.4.0",
     ],
     author="Danila Kurganov",             # Your name
-    author_email="dan.kurg@gmail.com",  # Your email
+    author_email="dan.kurg@gmail.com",    # Your email
     description="Automatic Differentiation in Python using Numpy/Numba",
     long_description=open("README.md").read(),
     url="https://github.com/baubels/danila-grad",
+    project_urls={
+        "Documentation": "https://baubels.github.io/danila-grad/",
+        "Source Code": "https://github.com/baubels/danila-grad",
+        "Issue Tracker": "https://github.com/baubels/danila-grad/issues",
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.10',
+    extras_require={
+        "examples": ["dill", "tqdm"],
+        "dev": ["pytest", "sphinx", "torch", "numpy==1.23.0"],  # Add optional development dependencies
+    },
 )
-
