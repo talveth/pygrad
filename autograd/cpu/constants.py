526f9f7396f445d16b096c3da564d485c712456d
Danila Kurganov <dan.kurg@gmail.com>
2024-11-29 13:37:46 +0100

    64

--- autograd/cpu/constants.py
+++ autograd/cpu/constants.py
@@ -6,7 +6,7 @@ Module storing library-wide global constants. Define the precision of .value and
 # define what data type you want
 import numpy as np
 
-PRECISION = np.float32
+PRECISION = np.float64
 # one of np.float32, np.float64, np.float128
 # if not using softmax .grad for 3D tensors, can use np.float16
 # np.float16 will have too many precision errors to run the transformer model, use >32.
